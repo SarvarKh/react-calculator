33ad7f79326db684e1ec67422bfa89c93a4a6fc4
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-07-17 17:10:22 +0500

    Clear up App.js and ButtonPanel.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-import calculate from '../logic/calculate';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      total: null,
-      next: null,
-      operation: null,
+      // total: null,
+      // next: null,
+      // operation: null,
     };
   }
 
   render() {
-    calculate(this.state, '=');
     return (
       <>
         <Display />
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -2,37 +2,37 @@
 import React from 'react';
 import Button from './Button';
 
-export default function ButtonPanel(props) {
+export default function ButtonPanel() {
   return (
     <>
       <div>
-        <Button name="AC" onClick={() => props.handleClick('AC')} />
-        <Button name="+/-" onClick={() => props.handleClick('+/-')} />
-        <Button name="%" onClick={() => props.handleClick('%')} />
-        <Button name="÷" onClick={() => props.handleClick('÷')} />
+        <Button name="AC" />
+        <Button name="+/-" />
+        <Button name="%" />
+        <Button name="÷" />
       </div>
       <div>
-        <Button name="7" onClick={() => props.handleClick('7')} />
-        <Button name="8" onClick={() => props.handleClick('8')} />
-        <Button name="9" onClick={() => props.handleClick('9')} />
-        <Button name="X" onClick={() => props.handleClick('X')} />
+        <Button name="7" />
+        <Button name="8" />
+        <Button name="9" />
+        <Button name="X" />
       </div>
       <div>
-        <Button name="4" onClick={() => props.handleClick('4')} />
-        <Button name="5" onClick={() => props.handleClick('5')} />
-        <Button name="6" onClick={() => props.handleClick('6')} />
-        <Button name="-" onClick={() => props.handleClick('-')} />
+        <Button name="4" />
+        <Button name="5" />
+        <Button name="6" />
+        <Button name="-" />
       </div>
       <div>
-        <Button name="1" onClick={() => props.handleClick('1')} />
-        <Button name="2" onClick={() => props.handleClick('2')} />
-        <Button name="3" onClick={() => props.handleClick('3')} />
-        <Button name="+" onClick={() => props.handleClick('+')} />
+        <Button name="1" />
+        <Button name="2" />
+        <Button name="3" />
+        <Button name="+" />
       </div>
       <div>
-        <Button name="0" onClick={() => props.handleClick('0')} />
-        <Button name="," onClick={() => props.handleClick(',')} />
-        <Button name="=" onClick={() => props.handleClick('=')} />
+        <Button name="0" />
+        <Button name="," />
+        <Button name="=" />
       </div>
     </>
   );
