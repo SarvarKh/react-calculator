2e57b1f0bfd37c545c44271e85feede9daf3c068
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-07-18 10:40:20 +0500

    Delete redundant code from calculate.js

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -5,18 +5,12 @@ const calculate = (data, buttonName) => {
   const operations = ['+', '-', 'X', '÷'];
 
   if (buttonName === '+/-') {
-    newData.total *= (-1);
     newData.next = parseFloat(newData.next) * (-1);
   } else if (buttonName === 'AC') {
-    newData.total = '0';
     newData.next = '0';
-    newData.operation = '0';
   } else if (buttonName === '%') {
-    newData.total = '0';
     newData.next /= 100;
-    newData.operation = '0';
   } else if (operations.includes(buttonName)) {
-    console.log(newData.total, newData.next, newData.operation);
     newData.next = operate(newData.total, newData.next, newData.operation);
     newData.total = '';
     newData.operation = null;
