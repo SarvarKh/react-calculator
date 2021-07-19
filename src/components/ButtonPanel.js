6d3bfb717ab596799487e66e13e667bf4afb9ee9
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-07-19 10:02:00 +0500

    Fix linter errors

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Button from './Button';
 
 export default function ButtonPanel(props) {
@@ -37,3 +38,11 @@ export default function ButtonPanel(props) {
     </>
   );
 }
+
+ButtonPanel.propTypes = {
+  grantParentHandleState: PropTypes.func,
+};
+
+ButtonPanel.defaultProps = {
+  grantParentHandleState: '0',
+};
