8d6714141a9d27e316baf2e23f62f65d8b856993
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-07-21 09:50:01 +0500

    Setup Router and Call home page via Router.js

--- src/Routes.js
+++ src/Routes.js
@@ -4,14 +4,14 @@ import Home from './components/Home';
 import App from './components/App';
 import Quote from './components/Quote';
 
-const Routes = () => {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" component={Home} exact />
-                <Route path="./components/App" component={App} />
-                <Route path="./components/Quote" component={Quote} />
-            </Switch>
-        </BrowserRouter>
-    )
-}
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="./components/App" component={App} />
+      <Route path="./components/Quote" component={Quote} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
