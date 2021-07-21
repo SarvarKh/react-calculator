bcd0814cc667d55fd9923c1bb17e82807c2b78d5
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-07-21 10:41:14 +0500

    Add content for Home, Calculator, and Quote pages

--- src/components/App.js
+++ src/components/App.js
@@ -36,6 +36,8 @@ const App = () => {
 
   return (
     <>
+      <h1>Math Magicians</h1>
+      <h3>Let&apos;s do some math!</h3>
       <Display status={next} />
       <ButtonPanel grantParentHandleState={handleState} />
     </>
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,7 +3,11 @@ import React from 'react';
 export default function Quote() {
   return (
     <div>
-      <h2>Quote of the day!</h2>
+      <h1>Math Magicians</h1>
+      <div>
+        Mathematics is not about numbers, equations, computations,
+        or algorithms: it is about understanding. –William Paul Thurston
+      </div>
     </div>
   );
 }
