e4bc96acb529502659938eb0689746b902af4230
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-07-21 09:57:47 +0500

    Hanling error urls

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import App from './components/App';
 import Quote from './components/Quote';
+import Error from './Error';
 
 const Routes = () => (
   <BrowserRouter>
@@ -10,6 +11,7 @@ const Routes = () => (
       <Route path="/" component={Home} exact />
       <Route path="./components/App" component={App} />
       <Route path="./components/Quote" component={Quote} />
+      <Route component={Error} />
     </Switch>
   </BrowserRouter>
 );
