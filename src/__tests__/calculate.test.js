9a6f5d0df4324a128d9e5f915cc677f9ab182365
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-07-22 14:22:52 +0500

    Write tests for AC and =/- btns

--- src/__tests__/calculate.test.js
+++ src/__tests__/calculate.test.js
@@ -53,3 +53,53 @@ describe("Calculate methods: 'AC' and '+/-'", () => {
         expect(calculate(newObject, button)).toStrictEqual(result);
     })
 })
+
+describe("Calculate methods: 'AC' and '+/-'", () => {
+    test('Turns object next value to 0 if btn is AC', () => {
+        const newObject = {
+            next: '9',
+            total: '5',
+            operation: null
+        };
+        const button = 'AC';
+        const result = {
+            next: '0',
+            total: '5',
+            operation: null
+        };
+    
+        expect(calculate(newObject, button)).toStrictEqual(result);
+    });
+
+    test('Turns the integer to opposite format +/-', () => {
+        const newObject = {
+            next: '-3',
+            total: '15',
+            operation: null
+        };
+        const button = '+/-';
+        const result = {
+            next: '3',
+            total: '15',
+            operation: null
+        };
+    
+        expect(calculate(newObject, button)).toStrictEqual(result);
+    })
+
+    test('Turns the decimal to opposite format +/-', () => {
+        const newObject = {
+            next: '0.3',
+            total: '1',
+            operation: null
+        };
+        const button = '+/-';
+        const result = {
+            next: '-0.3',
+            total: '1',
+            operation: null
+        };
+    
+        expect(calculate(newObject, button)).toStrictEqual(result);
+    })
+})
