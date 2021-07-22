ce3419b8f9d39eb27873fbd780478c008971c43a
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-07-22 18:31:06 +0500

    Write Rtests for DOM testing with React testing library

--- src/__tests__/Quote.test.js
+++ src/__tests__/Quote.test.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Quote from '../components/Quote';
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import pretty from "pretty";
 
-it("renders correctly", () => {
-    const tree = renderer
-        .create(<Quote>Quote</Quote>)
-        .toJSON();
-    expect(tree).toMatchSnapshot();
-})
+import Quote from "../components/Quote";
+
+let container = null;
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("should render a Quote", () => {
+  act(() => {
+    render(<Quote />, container);
+  });
+
+  expect(pretty(container.innerHTML)).toMatchInlineSnapshot(`
+    "<div>
+      <div>Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding. –William Paul Thurston</div>
+    </div>"
+  `);
+});
